cbd55ce37f736974d7e0430b4175e0cfc1714334
Anush28Sharma <anush28sharma@gmail.com>
2025-07-24 00:49:25 +0530

    Fix: add missing await in updateAccountDetails for user update operation

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -263,7 +263,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
   if (!fullName || !email) {
     throw new ApiError(400, "all fields are required");
   }
-  const user = User.findByIdAndUpdate(
+  const user =await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set:{
